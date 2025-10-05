c2d35279d64f772281f6fe111d761cb565860b07
RioMahler <229354130+RioMahler@users.noreply.github.com>
2025-10-05 22:51:59 +0200

    Add user ability to determine grid size

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let containerSize = 100;
+let squareAmount = 50;
 let containerElement = document.querySelector("#container");
 
+let btn = document.querySelector("button").addEventListener("click", (e) => {
+  let userInput = prompt("How big should the grid be?");
+
+  if (userInput < 101) {
+    removeGrid();
+    squareAmount = userInput;
+    createGrid();
+  } else {
+    return;
+  }
+});
+
 function createGrid() {
-  for (let i = 0; i < containerSize; i++) {
+  for (let i = 0; i < squareAmount; i++) {
     const square = document.createElement("div");
 
     containerElement.appendChild(square);
-    for (let i = 0; i < containerSize; i++) {
+    for (let i = 0; i < squareAmount; i++) {
       const field = document.createElement("div");
       field.classList.add("square");
       field.addEventListener("mouseenter", (e) => {
@@ -15,6 +27,15 @@ function createGrid() {
 
       square.appendChild(field);
     }
+  }
+}
+
+function removeGrid() {
+  for (let i = 0; i < squareAmount; i++) {
+    let columns = containerElement.childNodes;
+    columns.forEach(function (listObject) {
+      listObject.remove();
+    });
   }
 }
 
