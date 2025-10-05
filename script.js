b24c8a61e84b0b10a29261cd12b3d5472bec4a09
RioMahler <229354130+RioMahler@users.noreply.github.com>
2025-10-05 22:28:14 +0200

    Change grid to use same sum of pixel at any size

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let container = [];
+let containerSize = 100;
 let containerElement = document.querySelector("#container");
 
 function createGrid() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < containerSize; i++) {
     const square = document.createElement("div");
 
     containerElement.appendChild(square);
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < containerSize; i++) {
       const field = document.createElement("div");
       field.classList.add("square");
       field.addEventListener("mouseenter", (e) => {
